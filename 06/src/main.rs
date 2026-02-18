ca434137a30c50f3d2ff6455a0ef9aea4d55f1cf
Nico Mendez <ttmendezleal@willamette.edu>
2026-02-18 15:23:42 -0800

    Hello!!!!

--- 06/src/main.rs
+++ 06/src/main.rs
@@ -3,6 +3,21 @@
 
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
+    
+    let vga = 0xb8000 as *mut u8;
+
+    let ints: [i32; 3] = [1819043144, 1870078063, 560229490];
+
+    let pointer = &ints as *const i32 as *const u8;
+
+
+    for i in 0..12 {
+        unsafe{
+            *((vga as usize + 2 * i) as *mut u8) = *((pointer as usize + i) as *const u8);
+            *((vga as usize + 2 * i + 1) as *mut u8) = 0xF;
+        }
+    }
+
     loop {}
 }
 
