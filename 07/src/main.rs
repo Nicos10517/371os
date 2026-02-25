82d7cd6f7bffab8bb3d754c691703498fcb516ae
Nico Mendez <ttmendezleal@willamette.edu>
2026-02-25 14:54:59 -0800

    Working Step 1

--- 07/src/main.rs
+++ 07/src/main.rs
@@ -2,16 +2,17 @@
 #![no_main]
 
 mod vga;
+mod colors;
 
 #[panic_handler]
 #[allow(unconditional_recursion)]
 fn panic(info: &core::panic::PanicInfo) -> ! {
-    panic(info)
+    println!("{}", info);
+    loop {}
 }
 
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
-    println!("Hello World{}", "!");
-    loop {}
-
+    colors::colors();
+    loop{}
 }
