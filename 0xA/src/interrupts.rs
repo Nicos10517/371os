da4574fcc99f50eebfbddefe042fad716f160ecc
Nico Mendez <ttmendezleal@willamette.edu>
2026-04-08 14:50:16 -0700

    Working post double, pre-inerrupt

---
+++ 0xA/src/interrupts.rs
@@ -0,0 +1,34 @@
+use lazy_static::lazy_static;
+
+
+lazy_static! {
+    static ref IDT: x86_64::structures::idt::InterruptDescriptorTable = {
+        let mut idt = x86_64::structures::idt::InterruptDescriptorTable::new();
+        idt.breakpoint.set_handler_fn(breakpoint_handler);
+        unsafe {
+            idt.double_fault
+                .set_handler_fn(double_fault_handler)
+                .set_stack_index(crate::gdt::DOUBLE_FAULT_IST_INDEX as u16);
+        }
+        idt
+    };
+}
+
+pub fn init_idt() {
+    IDT.load();
+}
+
+extern "x86-interrupt" fn breakpoint_handler (stack_frame: x86_64::structures::idt::InterruptStackFrame) {
+    crate::println!("EXCEPTION: BREAKPOINT\n{:#?}", stack_frame);
+}
+
+extern "x86-interrupt" fn double_fault_handler ( stack_frame: x86_64::structures::idt::InterruptStackFrame, error_code: u64) -> ! {
+    assert!(error_code == 0);
+    panic!("EXCEPTION: DOUBLE FAULT\n{:#?}", stack_frame);
+}
+
+/*pub const PIC_1_OFFSET: u8 = 32;
+pub const PIC_2_OFFSET: u8 = PIC_1_OFFSET + 8;
+
+pub static mut PICS: pic8259::ChainedPics =
+    unsafe { pic8259::ChainedPics::new(PIC_1_OFFSET, PIC_2_OFFSET) };*/
