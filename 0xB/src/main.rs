2d5f0119b0d32b201219d615ad7c26d83eebe975
Nico Mendez <ttmendezleal@willamette.edu>
2026-05-03 14:26:04 -0700

    Working on Mapping, Pre-Allocating Frames

---
+++ 0xB/src/main.rs
@@ -0,0 +1,55 @@
+#![no_std]
+#![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(osirs::_test_runner)]
+#![reexport_test_harness_main = "test_main"]
+#![allow(static_mut_refs)]
+
+mod vga;
+
+
+
+#[cfg(not(test))]
+#[panic_handler]
+fn panic(info: &core::panic::PanicInfo) -> ! {
+    println!("{}", info);
+    loop {}
+}
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &core::panic::PanicInfo) -> ! {
+    osirs::test_panic_handler(info);
+    /*osirs::halt();*/
+}
+
+
+#[unsafe(no_mangle)]
+pub extern "C" fn _start(boot_info: &'static bootloader::BootInfo) -> ! {
+
+    let offset = x86_64::VirtAddr::new(boot_info.physical_memory_offset);
+    let mut mapper = unsafe { osirs::memory::init(offset) };
+    let mut frame_allocator = osirs::memory::EmptyFrameAllocator;
+
+    // map an unused page
+    let page = x86_64::structures::paging::Page::containing_address(x86_64::VirtAddr::new(0));
+    osirs::memory::create_example_mapping(page, &mut mapper, &mut frame_allocator);
+
+    // write the string `New!` to the screen through the new mapping
+    let ptr: *mut u64 = page.start_address().as_mut_ptr();
+    unsafe { ptr.write_volatile(0x_f021_f077_f065_f04e) };
+        
+    /*osirs::halt();*/
+    
+    /*
+    let ptr = 0xdeadbeef as *mut u8;
+    unsafe { *ptr = 42; }
+    println!("It did not crash!");
+    */
+    
+    #[cfg(test)]
+    test_main();
+
+    loop{}
+}
+
---
+++ 0xB/src/memory.rs
@@ -0,0 +1,74 @@
+use x86_64::structures::paging::PageTable;
+
+
+
+pub unsafe fn active_level_4_table(offset: x86_64::VirtAddr)
+-> &'static mut x86_64::structures::paging::PageTable
+{
+    unsafe {
+        let (frame, _) = x86_64::registers::control::Cr3::read();
+    
+        let phys = frame.start_address();
+        let virt = offset + phys.as_u64();
+        let ptr: *mut PageTable = virt.as_mut_ptr();
+    
+        return &mut *ptr;
+    }
+}
+
+pub unsafe fn translate_addr(addr: x86_64::VirtAddr, offset: x86_64::VirtAddr)
+    -> Option<x86_64::PhysAddr>
+{
+    unsafe {
+        let (l4, _) = x86_64::registers::control::Cr3::read();
+        let mut frame = l4;
+        let indices = [addr.p4_index(), addr.p3_index(), addr.p2_index(), addr.p1_index()];
+        for &index in &indices {
+            let virt = offset + frame.start_address().as_u64();
+            let ptr: *const x86_64::structures::paging::PageTable = virt.as_ptr();
+            frame = match &(&*ptr)[index].frame() {
+                Ok(x) => *x,
+                Err(_) => return None,
+            };
+        }
+        return Some(frame.start_address() + u64::from(addr.page_offset())); 
+    }
+}
+
+pub unsafe fn init(offset: x86_64::VirtAddr) -> x86_64::structures::paging::OffsetPageTable<'static> {
+    unsafe {
+        let l4 = active_level_4_table(offset);
+        x86_64::structures::paging::OffsetPageTable::new(l4, offset)
+    }
+}
+
+pub fn create_example_mapping(
+    page: x86_64::structures::paging::Page,
+    mapper: &mut x86_64::structures::paging::OffsetPageTable,
+    frame_allocator: &mut impl x86_64::structures::paging::FrameAllocator<
+        x86_64::structures::paging::Size4KiB,
+    >,
+) {
+    let frame =
+        x86_64::structures::paging::PhysFrame::containing_address(x86_64::PhysAddr::new(0xb8000));
+    let flags = x86_64::structures::paging::PageTableFlags::PRESENT
+        | x86_64::structures::paging::PageTableFlags::WRITABLE;
+
+    let map_to_result = unsafe {
+        use x86_64::structures::paging::Mapper;
+        mapper.map_to(page, frame, flags, frame_allocator)
+    };
+    map_to_result.expect("map_to failed").flush();
+}
+
+pub struct EmptyFrameAllocator;
+
+unsafe impl x86_64::structures::paging::FrameAllocator<x86_64::structures::paging::Size4KiB>
+    for EmptyFrameAllocator
+{
+    fn allocate_frame(&mut self) -> Option<x86_64::structures::paging::PhysFrame> {
+        None
+    }
+}
+
+
